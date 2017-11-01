d3b26b3c67ee719c8917534b74470c2cc3eb77ef
Sandor Dargo <sandordargo@users.noreply.github.com>
2017-11-01 22:15:02 +0100

    feature: Add links to node details

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,24 +5,33 @@ var svg = d3.select("svg").attr("width", width).attr("height", height);
 
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+var simulation;
 
 function graph() {
   return getJsonFrom("/data/data");
 }
 
-var nodes = graph()["nodes"]
-var links = graph()["links"]
+var nodes;
+var links;
+
+function refreshLocalGraph() {
+    var localGraph = graph();
+    nodes = localGraph["nodes"];
+    links = localGraph["links"];
+}
 
 function reset() {
-  console.info("update!")
-  nodes = graph()["nodes"];
-  links = graph()["links"];
+  refreshLocalGraph();
   simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(function(edge) {
+        if (edge.source.type == "WineRegion" || edge.target.type == "WineRegion" ) {
+            return 25;
+        } else if ((edge.source.type == "WineSubRegion" && edge.target.type == "Grape")
+            || (edge.source.type == "Grape" && edge.target.type == "WineSubRegion")) {
+            return 25;
+        }
+        return 75;
+    }))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
   update();
@@ -33,64 +42,26 @@ function hard_reset() {
   nodes = [];
   links = [];
   simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(function(edge) {
+        if (edge.source.type == "WineRegion" || edge.target.type == "WineRegion" ) {
+            return 25;
+        } else if ((edge.source.type == "WineSubRegion" && edge.target.type == "Grape")
+            || (edge.source.type == "Grape" && edge.target.type == "WineSubRegion")) {
+            return 25;
+        }
+        return 75;
+    }))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
   update();
 }
+
 var zoom_handler;
 
-
-function clickcancel() {
-  var event = d3.dispatch('click', 'dblclick');
-
-  function cc(selection) {
-    var down,
-      tolerance = 5,
-      last,
-      wait = null;
-    // euclidean distance
-    function dist(a, b) {
-      return Math.sqrt(Math.pow(a[0] - b[0], 2), Math.pow(a[1] - b[1], 2));
-    }
-    selection.on('mousedown', function() {
-      down = d3.mouse(document.body);
-      last = +new Date();
-    });
-    selection.on('mouseup', function() {
-      if (dist(down, d3.mouse(document.body)) > tolerance) {
-        return;
-      } else {
-        if (wait) {
-          window.clearTimeout(wait);
-          wait = null;
-          event.call('dblclick', d3.event);
-        } else {
-          wait = window.setTimeout((function(e) {
-            return function() {
-              event.call('click', e);
-              wait = null;
-            };
-          })(d3.event), 300);
-        }
-      }
-    });
-    // selection.on('start', function() {dragstarted(this)});
-    //     .on("drag", dragged)
-    //     .on("end", dragended));
-  };
-  cc.on = function() {
-    var value = event.on.apply(event, arguments);
-    return value === event ? cc : value;
-  };
-  return cc;
-}
-
 function update() {
-  console.info("update!")
   svg.selectAll("*").remove();
-  links_to_display = links
-  nodes_to_display = nodes
+  links_to_display = links;
+  nodes_to_display = nodes;
   
   var link = svg.append("g")
     .attr("class", "links")
@@ -99,8 +70,6 @@ function update() {
     .enter().append("line")
     .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-    
-      
   var node = svg.append("g")
     .attr("class", "nodes")
     .selectAll("circle")
@@ -128,7 +97,6 @@ function update() {
             doubleclick(d)
         });
 
-    
     node.call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
@@ -140,11 +108,8 @@ function update() {
 
   simulation
     .nodes(nodes_to_display)
-    .on("tick", ticked);
-
-  simulation.force("link")
-      .links(links_to_display);
-
+    .on("tick", ticked).force("link")
+    .links(links_to_display);
 
   zoom_handler = d3.zoom().on("zoom", zoom_actions);
 
@@ -154,7 +119,6 @@ function update() {
   }
 
   zoom_handler(svg);
-
 
   function ticked(d) {
     link
@@ -170,7 +134,6 @@ function update() {
 };
 
 
-
 function zoomInClick() {
   zoom_handler.scaleBy(svg, 1.2);
 };
@@ -181,23 +144,12 @@ function zoomOutClick() {
 };
 
 function reCenter() {
-  console.info("center");
-  // simulation = d3.forceSimulation()
-  //   .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  //   .force("charge", d3.forceManyBody())
-  //   // .force("gravity", 1)
-  //   .force("center", d3.forceCenter(width / 2, height / 2));
   update();
-  // simulation = d3.forceSimulation()
-  //   .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  //   .force("charge", d3.forceManyBody())
-  //   .force("center", d3.forceCenter(width / 2, height / 2));
 };
 
 function click(d)
 {
-    expandNode(d)
-
+    expandNode(d);
 }
 
 function doubleclick(d)
@@ -208,44 +160,71 @@ function doubleclick(d)
     document.getElementById("node-details").innerHTML = "Node details: " + getNodeDetails(d);
 }
 
+
+function getGrape(id) {
+    info = getJsonFrom("/data/subregions_of_grape/" + id);
+    for (node in info["nodes"]) {
+        if (info["nodes"][node].id == id) {
+            return info["nodes"][node];
+        }
+    }
+}
+
+function getRegion(id) {
+    info = getJsonFrom("/data/regions/" + id);
+    for (node in info["nodes"]) {
+        if (info["nodes"][node].id == id) {
+            return info["nodes"][node];
+        }
+    }
+}
+
+function getSubregion(id) {
+    info = getJsonFrom("/data/subregion_with_grapes_and_parent_and_wineries/" + id);
+    for (node in info["nodes"]) {
+        if (info["nodes"][node].id == id) {
+            return info["nodes"][node];
+        }
+    }
+}
+
 function getNodeDetails(node) {
   if (node.type == "WineRegion") {
     info = getJsonFrom("/data/regions/" + node.id);
     subregions = "<br>Subregions included: "
-    for (node in info["nodes"]) {
-      if (info["nodes"][node].type == "WineSubRegion") {
-        subregions += info["nodes"][node].caption + ", ";
+    for (node_id in info["nodes"]) {
+      if (info["nodes"][node_id].type == "WineSubRegion") {
+        link = "<a href=\"javascript:doubleclick(getSubregion(" + info["nodes"][node_id].id + "));\">" + info["nodes"][node_id].caption + "</a>"
+        subregions += link + ", ";
       }
-
     }
     return subregions.slice(0, -2);
-
   } else if (node.type == "WineSubRegion") {
     info = getJsonFrom("/data/subregion_with_grapes_and_parent_and_wineries/" + node.id);
     parent = ""
     grapes = "Grapes produced: "
     wineries = "Wineries in subregion: "
-    for (node in info["nodes"]) {
-      if (info["nodes"][node].type == "WineRegion") {
-        parent = "<br>Parent region: " + info["nodes"][node].caption;
-      } else if (info["nodes"][node].type == "Grape") {
-        grapes += info["nodes"][node].caption + ", ";
-      } else if (info["nodes"][node].type == "Winery") {
-        wineries += info["nodes"][node].caption + ", ";
+    for (node_id in info["nodes"]) {
+      if (info["nodes"][node_id].type == "WineRegion") {
+        link = "<a href=\"javascript:doubleclick(getRegion(" + info["nodes"][node_id].id + "));\">" + info["nodes"][node_id].caption + "</a>"
+        parent = "<br>Parent region: " + link;
+      } else if (info["nodes"][node_id].type == "Grape") {
+        link = "<a href=\"javascript:doubleclick(getGrape(" + info["nodes"][node_id].id + "));\">" + info["nodes"][node_id].caption + "</a>"
+        grapes += link + ", ";
+      } else if (info["nodes"][node_id].type == "Winery") {
+        wineries += info["nodes"][node_id].caption + ", ";
       }
     }
-
     return parent + "<br>" + grapes.slice(0, -2) + "<br>" + wineries.slice(0, -2);
   } else if (node.type == "Grape") {
     info = getJsonFrom("/data/subregions_of_grape/" + node.id);
     parentSubregions = "This grape is procuded at: "
-    for (node in info["nodes"]) {
-      if (info["nodes"][node].type == "WineSubRegion") {
-        parentSubregions += info["nodes"][node].caption + ", ";
+    for (node_id in info["nodes"]) {
+      if (info["nodes"][node_id].type == "WineSubRegion") {
+        link = "<a href=\"javascript:doubleclick(getSubregion(" + info["nodes"][node_id].id + "));\">" + info["nodes"][node_id].caption + "</a>"
+        parentSubregions += link + ", ";
       }
-
     }
-
     return parentSubregions.slice(0, -2);
   } else if (node.type == "Winery") {
     var url;
@@ -256,7 +235,7 @@ function getNodeDetails(node) {
     } else {
       url = "<a href=\"" + node.url + "\">" + node.url + "</a>";
     }
-    return "Name: " + node.caption + "<br>Village: " + node.village + "<br>Url: " + url;
+    return "<br>Name: " + node.caption + "<br>Village: " + node.village + "<br>Url: " + url;
   }
   return "no details for nodes with type of " + node.type;
 }
@@ -287,60 +266,28 @@ function expandNode(d) {
       genericExpand(d, needed["nodes"], needed["links"]);
   } else if (d.type == "Grape" && (d.expanded == "false" || d.expanded == false)) {
     needed = getJsonFrom("/data/subregions_of_grape/" + d.id);
-    genericExpand(d, needed["nodes"], needed["links"])
-      
+    genericExpand(d, needed["nodes"], needed["links"]);
   } else if (d.expanded == true || d.expanded == "true") {
     collapseNode(d);  
   }
-
 };
 
 function genericExpand(d, new_nodes, new_links) {
-  //remove node to expand from new_nodes
-  for (node in new_nodes) {
-    console.info(new_nodes[node])
-    if (new_nodes[node].id == d.id) {
-      new_nodes.splice(node, 1);
-      console.info(new_nodes[node])
-    }
-  }
-
-  //do not add a node the second time that is already displayed
-  var i = 0;
-  var stop = false
-  while (i <= new_nodes.length + 1) {
-    for (node in new_nodes) {
-      for (old_node in nodes) {
-        if (new_nodes[node].id == nodes[old_node].id) {
-          new_nodes.splice(node, 1);
-          stop = true;
-          break;
-        } 
-      }
-    }
-    i++;
-  }
-
-  nodes = nodes.concat(new_nodes)
-  links  = links.concat(new_links)
+  mergeNodes(new_nodes);
+  mergeLinks(new_links);
   d.expanded = true;
-  if (new_links.length > 0 || new_nodes.length > 0) {
-    update();
-  }
   update();
 }
 
 function collapseNode(node) {
   if (node.type == "WineSubRegion") {
     to_clean_up = getJsonFrom("/data/subregion_with_grapes_and_parent_and_wineries/" + node.id);
-    genericCollapse(node, to_clean_up["nodes"], to_clean_up["links"], "WineSubRegion")
   } else if (node.type == "Grape") {
     to_clean_up = getJsonFrom("/data/subregions_of_grape/" + node.id);
-    genericCollapse(node, to_clean_up["nodes"], to_clean_up["links"], "Grape")
   } else if (node.type == "WineRegion") {
     to_clean_up = getJsonFrom("/data/regions/" + node.id);
-    genericCollapse(node, to_clean_up["nodes"], to_clean_up["links"], "Grape")
   }
+  genericCollapse(node, to_clean_up["nodes"], to_clean_up["links"], node.type)
 };
 
 function genericCollapse(node, nodes_to_remove, links_to_remove, node_type) {
@@ -358,7 +305,6 @@ function genericCollapse(node, nodes_to_remove, links_to_remove, node_type) {
 
   node.expanded = false;
   update();
-
 }
 
 function filterArray(source, filter) {
@@ -369,19 +315,13 @@ function filterArray(source, filter) {
         console.info(temp[filter[i]])
     }
 
-    console.info(temp)
-
     for (i = 0; i < source.length; i++) {
         id = source[i].source.id + "-" + source[i].target.id ;
-        console.info(id);
         if (temp.indexOf(id) == -1) {
-            console.info(id + " is not in temp");
             result.push(source[i]);
-        } else {
-          console.info(id + " is removed");
         }
     }
-    return(result);
+    return result;
 }
 
 function isLinkedToMoreThanOne(node) {
@@ -404,25 +344,27 @@ function getJsonFrom(uri) {
 
 document.getElementById("wineRegions").onchange = function()
 {
-  addItemFrom("/data/regions/" + this.value);
+  addItemFrom("/data/regions/" + this.value, this.value);
 };
 
 document.getElementById("wineSubregions").onchange = function()
 {
-  addItemFrom("/data/subregion_with_grapes_and_parent/" + this.value);
+  addItemFrom("/data/subregion_with_grapes_and_parent_and_wineries/" + this.value, this.value);
 };
 
 document.getElementById("grapes").onchange = function()
 {
-  addItemFrom("/data/subregions_of_grape/" + this.value);
+  addItemFrom("/data/subregions_of_grape/" + this.value, this.value);
 };
 
-function addItemFrom(uri)
+function addItemFrom(uri, id)
 {
   needed = getJsonFrom(uri);
-  // for (node in needed["nodes"]) {
-  //   needed["nodes"][node].expanded = true;
-  // }
+   for (node in needed["nodes"]) {
+     if (needed["nodes"][node].id == id) {
+        needed["nodes"][node].expanded = true;
+     }
+   }
   mergeNodes(needed["nodes"]);
   mergeLinks(needed["links"]);
   update();
@@ -431,10 +373,11 @@ function addItemFrom(uri)
 function mergeNodes(new_nodes) {
   for (new_node in new_nodes) {
 
-    already_there = false;
+    var already_there = false;
     for (node in nodes) {
       if (nodes[node].id == new_nodes[new_node].id) {
-        already_there = true
+        already_there = true;
+        break;
       } 
     }
 
@@ -446,10 +389,11 @@ function mergeNodes(new_nodes) {
 
 function mergeLinks(new_links)   {
   for (new_link in new_links) {
-    already_there = false;
+    var already_there = false;
     for (link in links) {
       if (links[link].source == new_links[new_link].source && links[link].target == new_links[new_link].target) {
-        already_there = true
+        already_there = true;
+        break;
       } 
     }
 
