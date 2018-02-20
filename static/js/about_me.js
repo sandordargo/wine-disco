dbd7944fd5522c16c47dfda6ed341da89e494096
Sandor Dargo <sandordargo@users.noreply.github.com>
2018-02-20 09:06:16 +0100

    feature: Add About Me page

---
+++ static/js/about_me.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById('aboutMeModal');
+var btn = document.getElementById("aboutMeButton");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
